237a5d94657b7b6e131ba61562b58614f8c8bd02
Thai Si <hsthai.chp@gmail.com>
2026-03-30 06:33:48 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import CustomerManager from './pages/CustomerManager';
-import Login from './pages/Login';
-import ChangePassword from './pages/ChangePassword';
 import SparePartModal from './pages/SparePartModal';
+import ChangePassword from './pages/ChangePassword';
+import Settings from './pages/Settings';
+import Login from './pages/Login';
 import Index from './pages/Index';
 import Home from './pages/Home';
-import Settings from './pages/Settings';
-import MainBoard from './pages/MainBoard';
 import StaffManager from './pages/StaffManager';
+import MainBoard from './pages/MainBoard';
 
 function App() {
   return (
@@ -15,14 +15,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
         <Route path="/CustomerManager" element={<CustomerManager />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/ChangePassword" element={<ChangePassword />} />
         <Route path="/SparePartModal" element={<SparePartModal />} />
+        <Route path="/ChangePassword" element={<ChangePassword />} />
+        <Route path="/Settings" element={<Settings />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="/Index" element={<Index />} />
         <Route path="/Home" element={<Home />} />
-        <Route path="/Settings" element={<Settings />} />
-        <Route path="/MainBoard" element={<MainBoard />} />
         <Route path="/StaffManager" element={<StaffManager />} />
+        <Route path="/MainBoard" element={<MainBoard />} />
       </Routes>
     </BrowserRouter>
   )
