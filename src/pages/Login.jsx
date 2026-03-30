6108c520ed305c95a06dc866565e07b92f2d7a4b
Thai Si <hsthai.chp@gmail.com>
2026-03-30 07:36:09 +0000

    Update app pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
 export default function Login() {
   const navigate = useNavigate();
-  useEffect(() => {
-    navigate("/Home", { replace: true });
-  }, []);
+  useEffect(() => { navigate("/Home", { replace: true }); }, []);
   return null;
 }
