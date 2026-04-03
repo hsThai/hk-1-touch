cdcd34d1bd807bb4fc24ae0a3d0bf18e5a1ed93b
Thai Si <hsthai.chp@gmail.com>
2026-04-03 02:41:01 +0000

    File changes

--- src/pages/pb.jsx
+++ src/pages/pb.jsx
@@ -204,3 +204,5 @@ export async function testConnection(url) {
     return res.ok;
   } catch { return false; }
 }
+
+export default { getPbUrl, setPbUrl, setAuth, getAuth, clearAuth, pbAuth, Staff, RepairOrder, RepairChat, Notification, Customer, SparePart, SparePartUsage, AppSettings, uploadFile, subscribeCollection, pbSettings, testConnection };
